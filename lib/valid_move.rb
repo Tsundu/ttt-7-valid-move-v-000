2c7426b5c5118245d66d89ce9331a754c55543a1
Daniel Osment <osment.tsundu@gmail.com>
2020-01-07 08:37:32 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
   index = index.to_i
-  if !position_taken? && (index >=0 && index <= 8)  
+  if !position_taken?(board, index) && (index >=0 && index <= 8)  
     TRUE
   else
     FALSE
